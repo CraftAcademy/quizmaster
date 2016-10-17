4d8986074d7a46be0b86778ca4b8dc8a2cabfd86
Amber <amber@amberwilkie.com>
2016-10-17 21:46:58 +0200

    Write acceptance test for presence of questions on Quiz page

--- features/step_definitions/question_steps.rb
+++ features/step_definitions/question_steps.rb
@@ -1,5 +1,6 @@
-Given(/^the following questions exist:$/) do |table|
-  table.hashes.each do |body, answer|
-    FactoryGirl.create(:question, body: body, answer: answer)
+Given(/^the following questions exist in "([^"]*)":$/) do |quiz_name, table|
+  quiz = Quiz.find_by(name: quiz_name)
+  table.hashes.each do |question|
+    FactoryGirl.create(:question, body: question[:body], answer: question[:answer], quiz: quiz)
   end
 end
