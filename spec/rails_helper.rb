8b293b5b4aae8a92ac67e7836d7b846184681458
blokkinen <viktoria_blok@hotmail.com>
2016-10-17 15:34:24 +0200

    removed comments in database.yml

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,22 +11,22 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
-end
 
-Shoulda::Matchers.configure do |config|
+  Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
       with.library :rails
     end
+  end
 
-    config.before(:suite) do
-      DatabaseCleaner.strategy = :transaction
-      DatabaseCleaner.clean_with(:truncation)
-    end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-    config.around(:each) do |example|
-      DatabaseCleaner.cleaning do
-        example.run
-      end
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
     end
+  end
 end
