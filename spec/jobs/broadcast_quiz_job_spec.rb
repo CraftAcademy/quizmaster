dedf10d2ced8a8e258dd48308a77811871da5ba5
tochman <thomas@craftacademy.se>
2016-10-18 21:58:47 +0200

    adda spec for BroadcastQuizJob

--- spec/jobs/broadcast_quiz_job_spec.rb
+++ spec/jobs/broadcast_quiz_job_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe BroadcastQuizJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'matches with enqueued job' do
+    ActiveJob::Base.queue_adapter = :test
+    expect {
+      BroadcastQuizJob.perform_later
+    }.to have_enqueued_job(BroadcastQuizJob)
+  end
 end
