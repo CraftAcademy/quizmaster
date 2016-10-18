d5ee751b8043514aa96fee470c021d3dffb17c6f
tochman <thomas@craftacademy.se>
2016-10-18 10:05:03 +0200

    WIP adds scenario for accessing the quiz page as a team

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
   namespace :quizmaster do
     resources :quiz, controller: :quizzes, only: [:show]
   end
+
+  resources :quiz, controller: :games, only: [:index, :show] do
+  end
+
+  post 'access_quiz' , controller: :games, action: :access_quiz, as: :access_quiz
+
 end
