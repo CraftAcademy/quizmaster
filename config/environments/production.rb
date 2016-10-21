81ca40e77d0b68d847c53b34bd62970dd57372eb
Raoul DIFFOUO <diraulo@gmail.com>
2016-10-21 10:05:05 +0200

    update allowed origins and support both http and https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,9 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://quizmasterone.herokuapp.com/cable'
-  config.action_cable
-        .allowed_request_origins = ['https://quizmasterone.herokuapp.com']
+  config.action_cable.allowed_request_origins = [
+    'https://quizmasterone.herokuapp.com', %r{(http|https):\/\/quizmasterone.*}
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
