6632c384da2facc6210e091cf32c29fdcca8d043
Amber <amber@amberwilkie.com>
2016-10-22 20:00:50 +0200

    Add is_winner column to Team

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020141421) do
+ActiveRecord::Schema.define(version: 20161022175950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,9 @@ ActiveRecord::Schema.define(version: 20161020141421) do
   create_table "teams", force: :cascade do |t|
     t.string   "name"
     t.integer  "quiz_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "is_winner",  default: false
     t.index ["quiz_id"], name: "index_teams_on_quiz_id", using: :btree
   end
 
