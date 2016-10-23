bed7d0e6d09e5140315b995e18973134eea1b34c
Amber <amber@amberwilkie.com>
2016-10-23 18:01:14 +0200

    Add is_started column to Quiz

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023154619) do
+ActiveRecord::Schema.define(version: 20161023160023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,8 +39,9 @@ ActiveRecord::Schema.define(version: 20161023154619) do
   create_table "quizzes", force: :cascade do |t|
     t.string   "name"
     t.string   "code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "is_started", default: false
   end
 
   create_table "teams", force: :cascade do |t|
