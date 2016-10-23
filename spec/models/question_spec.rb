1a7409f42de2db5e154ed3509494bd0758a77064
Amber <amber@amberwilkie.com>
2016-10-23 17:49:13 +0200

    Add unit test for is_sent attr

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Question, type: :model do
   describe 'DB table' do
     it { is_expected.to have_db_column :body }
     it { is_expected.to have_db_column :answer }
+    it { is_expected.to have_db_column :is_sent }
     it { is_expected.to belong_to :quiz }
     it { is_expected.to have_many :answers }
   end
