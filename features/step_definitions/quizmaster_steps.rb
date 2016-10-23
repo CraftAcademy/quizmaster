bcc29083514c0d8717cbf83975d7d33622284e87
Amber <amber@amberwilkie.com>
2016-10-23 10:31:44 +0200

    Fix bug with correct button

--- features/step_definitions/quizmaster_steps.rb
+++ features/step_definitions/quizmaster_steps.rb
@@ -3,6 +3,13 @@ When(/^I press the "([^"]*)" button for question "([^"]*)"$/) do |button, questi
   within("#question#{question.id}") { click_link_or_button button }
 end
 
+When(/^I press the correct button for question "([^"]*)"$/) do |question|
+  question = Question.find_by(body: question)
+  find("#correct_button_#{question.id}").trigger('click')
+  # click_link_or_button "#correct_button_#{question.id}"
+  # within("#question#{question.id}") { click_link_or_button button }
+end
+
 Then(/^I should be on the correction page for "([^"]*)"$/) do |question|
   question = Question.find_by(body: question)
   expect(current_path).to eq "/quizmaster/quiz/answers/#{question.id}"
