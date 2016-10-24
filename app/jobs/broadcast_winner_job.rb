8723547336f4c83a87aaa1e68b7ab5d87e6e8e5b
thesuss <lullisuss@gmail.com>
2016-10-24 13:55:43 +0200

    created winner job

---
+++ app/jobs/broadcast_winner_job.rb
@@ -0,0 +1,12 @@
+class BroadcastWinnerJob < ApplicationJob
+  queue_as :default
+
+  def perform(data)
+    ActionCable.server.broadcast 'quiz_channel', create_partial(data)
+  end
+
+  def create_partial(data)
+    ApplicationController.renderer.render(partial: './partials/winner', locals: {data: data})
+  end
+end
+
