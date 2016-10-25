c242ca5e8d51cc18c03be2969468b05840581363
Amber <amber@amberwilkie.com>
2016-10-25 09:15:30 +0200

    Comment out everything in mobile.js

--- app/assets/javascripts/channels/mobile.js
+++ app/assets/javascripts/channels/mobile.js
@@ -1,35 +1,35 @@
 App.quiz_mobile = App.mobile.subscriptions.create("QuizMobileChannel", {
-    collection: function () {
-        return $("#message");
-    },
-
-    connected: function () {
-      console.log('mobile connected');
-        // Called when the subscription is ready for use on the server
-    },
-
-    disconnected: function () {
-        // Called when the subscription has been terminated by the server
-    },
-
-    received: function (data) {
-        // Called when there's incoming data on the websocket for this channel
-        return this.printMessage(data)
-        // return "Hallo there!";
-    },
-
-    printMessage: function (data) {
-        if (data.welcome == "true") {
-            return this.collection().html(
-                "<p>" + data.message + "</p>"
-            );
-        } else {
-            return this.collection().html(data);
-        }
-    },
-    submitAnswer: function (message) {
-        return this.perform('submit_answer', {
-            message: message
-        });
-    }
+    // collection: function () {
+    //     return $("#message");
+    // },
+    //
+    // connected: function () {
+    //   console.log('mobile connected');
+    //     // Called when the subscription is ready for use on the server
+    // },
+    //
+    // disconnected: function () {
+    //     // Called when the subscription has been terminated by the server
+    // },
+    //
+    // received: function (data) {
+    //     // Called when there's incoming data on the websocket for this channel
+    //     return this.printMessage(data)
+    //     // return "Hallo there!";
+    // },
+    //
+    // printMessage: function (data) {
+    //     if (data.welcome == "true") {
+    //         return this.collection().html(
+    //             "<p>" + data.message + "</p>"
+    //         );
+    //     } else {
+    //         return this.collection().html(data);
+    //     }
+    // },
+    // submitAnswer: function (message) {
+    //     return this.perform('submit_answer', {
+    //         message: message
+    //     });
+    // }
 });
