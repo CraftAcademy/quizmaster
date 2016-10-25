a91086a315dfd70ba6454bb82537bc896d135781
Amber <amber@amberwilkie.com>
2016-10-25 17:43:41 +0200

    Remove comments and unused step

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -3,11 +3,6 @@ module ApplicationCable
     identified_by :quiz_id
 
     def connect
-      # This can all be replaced by       self.quiz_id = cookies['quiz_id']
-      # except that we all have cookies already in our browsers and you need to create a new team to establish the quiz_id cookie
-      # team = Team.find(cookies['team_id'])
-      # quiz_id = team.quiz.id
-      # self.quiz_id = quiz_id
       self.quiz_id = cookies['quiz_id']
     end
   end
