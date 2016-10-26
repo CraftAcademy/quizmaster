50d1fdc26082ac561d46429bcf1a9d6342ff8003
blokkinen <viktoria_blok@hotmail.com>
2016-10-26 16:45:15 +0200

    Spec tests for user and factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    
+    email "viktoria@quizmaster.com"
+    password "password"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB table' do
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :encrypted_password }
+  end
+
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :password }
+  end
+
+  describe 'Factory' do
+    it 'should have valid Factory' do
+      expect(FactoryGirl.create(:user)).to be_valid
+    end
+  end
 end
