f1d3eeec2c25d6efe9cfb1d722938fbc661af500
blokkinen <viktoria_blok@hotmail.com>
2016-10-26 14:29:38 +0200

    Created a user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023160023) do
+ActiveRecord::Schema.define(version: 20161026122801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,23 @@ ActiveRecord::Schema.define(version: 20161023160023) do
     t.index ["quiz_id"], name: "index_questions_on_quiz_id", using: :btree
   end
 
+  create_table "quizmasters", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_quizmasters_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_quizmasters_on_reset_password_token", unique: true, using: :btree
+  end
+
   create_table "quizzes", force: :cascade do |t|
     t.string   "name"
     t.string   "code"
@@ -51,6 +68,23 @@ ActiveRecord::Schema.define(version: 20161023160023) do
     t.datetime "updated_at",                 null: false
     t.boolean  "is_winner",  default: false
     t.index ["quiz_id"], name: "index_teams_on_quiz_id", using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   add_foreign_key "answers", "questions"
