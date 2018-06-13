9583d0e06ec435d47d1168b57d075b65764c40f7
tochman <thomas@craftacademy.se>
2018-06-13 19:14:34 +0200

    refactors rake task to set env var to use headless state in Chrome updates Coveralls settitn to use merged rails

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,6 @@
-require 'cucumber/rails'
 require 'coveralls'
 Coveralls.wear_merged!("rails")
+require 'cucumber/rails'
 
 ActionController::Base.allow_rescue = false
 begin
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+require 'coveralls'
+Coveralls.wear_merged!('rails')
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
